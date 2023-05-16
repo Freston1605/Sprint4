6165a9f6d6cc240a37139d9f9cdaa7e7f18f6e16
maofuentes <mfuentesoliva@comercioquinta.cl>
2023-05-15 21:31:11 -0400

    Marcelo 21:31

--- main.py
+++ main.py
@@ -26,31 +26,11 @@ class BaseDatos():
         
         with open(self.nombre_archivo, "w") as archivo_json:
             json.dump(data, archivo_json)
-class BaseDatos():
-    def __init__(self, nombre_archivo):
-        self.nombre_archivo = nombre_archivo
-        self.tablas = {}
-        self.cargar_db()
-
-    def cargar_db(self):
-        with open(self.nombre_archivo) as archivo_json:
-            data = json.load(archivo_json)
-            usuarios = [Usuario(u["id_usuario"], u["clave"], u["nombre"], u["tipo"], u["telefono"], u["edad"], u["correo"]) for u in data["Usuarios"]]
-            return usuarios
-
-    def guardar_db(self, usuarios, productos, bodegas, ventas):
-        data = {"Usuarios": []}
-        for u in usuarios:
-            data["Usuarios"].append({"id_usuario": u.id_usuario, "clave": u.clave, "nombre": u.nombre, "tipo": u.tipo, "telefono": u.telefono, "edad": u.edad, "correo": u.correo})
-        
-        with open(self.nombre_archivo, "w") as archivo_json:
-            json.dump(data, archivo_json)
 
 class Usuario():
 
     """Clase abstracta para todos los usuarios de la plataforma. Posee elementos comunes heredables a clases como Cliente o vendedor."""
 
-    def __init__(self, id_usuario, clave, nombre, tipo, telefono, edad, correo):
     def __init__(self, id_usuario, clave, nombre, tipo, telefono, edad, correo):
         self.id_usuario = id_usuario
         self.__clave = clave
@@ -332,3 +312,9 @@ class CarroDeCompras():
 
             valor_total += valor_item * unidades
         return valor_total
+
+
+db_completa = BaseDatos("basedatos.json")
+db_cargada = db_completa.cargar_db()
+usuarios = db_cargada[0]
+print(usuarios.nombre)
