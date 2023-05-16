a32ef2083d1e38e3a76f813a11ad801c32a818b1
maofuentes <mfuentesoliva@comercioquinta.cl>
2023-05-15 21:03:30 -0400

    Fusion 21:03

--- main.py
+++ main.py
@@ -1,20 +1,37 @@
 import json
 import datetime
 import os
-from basedatos import *
-
 
 opc_disponible = "1"
 opcion = 0
 usuarios = []
 data = []
 
+class BaseDatos():
+    def __init__(self, nombre_archivo):
+        self.nombre_archivo = nombre_archivo
+        self.tablas = {}
+        self.cargar_db()
+
+    def cargar_db(self):
+        with open(self.nombre_archivo) as archivo_json:
+            data = json.load(archivo_json)
+            usuarios = [Usuario(u["id_usuario"], u["clave"], u["nombre"], u["tipo"], u["telefono"], u["edad"], u["correo"]) for u in data["Usuarios"]]
+            return usuarios
+
+    def guardar_db(self, usuarios, productos, bodegas, ventas):
+        data = {"Usuarios": []}
+        for u in usuarios:
+            data["Usuarios"].append({"id_usuario": u.id_usuario, "clave": u.clave, "nombre": u.nombre, "tipo": u.tipo, "telefono": u.telefono, "edad": u.edad, "correo": u.correo})
+        
+        with open(self.nombre_archivo, "w") as archivo_json:
+            json.dump(data, archivo_json)
 
 class Usuario():
 
     """Clase abstracta para todos los usuarios de la plataforma. Posee elementos comunes heredables a clases como Cliente o vendedor."""
 
-    def __innit__(self, id_usuario: str, clave: str, nombre, tipo, telefono, edad, correo):
+    def __init__(self, id_usuario, clave, nombre, tipo, telefono, edad, correo):
         self.id_usuario = id_usuario
         self.__clave = clave
         self.nombre = nombre
@@ -79,3 +96,9 @@ class CarroDeCompras():
 
     def calcular_total():
         pass
+
+
+db_completa = BaseDatos("basedatos.json")
+db_cargada = db_completa.cargar_db()
+usuarios = db_cargada[0]
+print(usuarios.nombre)
